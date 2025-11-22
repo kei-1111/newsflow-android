e81dd3686cceee484d74e1717e2deed9fe0384fd
kei-1111 <kei_02_1111@icloud.com>
2025-11-22 19:54:22 +0900

    chore: :feature:homeで使用するHomeTopAppBarを作成

---
+++ feature/home/src/main/kotlin/io/github/kei_1111/newsflow/android/feature/home/component/HomeTopAppBar.kt
@@ -0,0 +1,54 @@
+package io.github.kei_1111.newsflow.android.feature.home.component
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import io.github.kei_1111.newsflow.android.core.designsystem.component.feature.NewsflowLogo
+import io.github.kei_1111.newsflow.android.core.designsystem.theme.NewsflowAndroidTheme
+import io.github.kei_1111.newsflow.android.core.ui.preview.ComponentPreviews
+import io.github.kei_1111.newsflow.android.feature.home.R
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3ExpressiveApi::class)
+@Composable
+internal fun HomeTopAppBar(
+    modifier: Modifier = Modifier,
+) {
+    TopAppBar(
+        title = { NewsflowLogo() },
+        modifier = modifier,
+        actions = {
+            IconButton(
+                onClick = { /* TODO: 記事検索機能を実装する際に作成 */ },
+                colors = IconButtonDefaults.iconButtonColors(
+                    contentColor = MaterialTheme.colorScheme.onSurface
+                )
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_search),
+                    contentDescription = null,
+                    modifier = Modifier.size(32.dp),
+                )
+            }
+        }
+    )
+}
+
+@Composable
+@ComponentPreviews
+private fun HomeTopAppBarPreview() {
+    NewsflowAndroidTheme {
+        Surface {
+            HomeTopAppBar()
+        }
+    }
+}
