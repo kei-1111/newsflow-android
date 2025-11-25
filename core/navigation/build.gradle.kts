015f1f06f2097a688774449ebc90ea4ab26ff81d
kei-1111 <kei_02_1111@icloud.com>
2025-11-25 14:41:24 +0900

    chore: :core:navigationにナビゲーションに必要なライブラリとプラグインを追加

--- core/navigation/build.gradle.kts
+++ core/navigation/build.gradle.kts
@@ -1,7 +1,13 @@
 plugins {
     alias(libs.plugins.newsflow.android.android.library.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
     namespace = "io.github.kei_1111.newsflow.android.core.navigation"
+}
+
+dependencies {
+    implementation(libs.androidx.navigation3.runtime)
+    implementation(libs.androidx.navigation3.ui)
 }
