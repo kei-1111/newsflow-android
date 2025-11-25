89862e20616b9c709996c72d75e3ceb47aab3e08
kei-1111 <kei_02_1111@icloud.com>
2025-11-25 21:52:37 +0900

    chore: :feature:viewerを作成

---
+++ feature/viewer/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    alias(libs.plugins.newsflow.android.android.feature)
+}
+
+android {
+    namespace = "io.github.kei_1111.newsflow.android.feature.viewer"
+}
+
+dependencies {
+    // Add your dependencies here
+}
