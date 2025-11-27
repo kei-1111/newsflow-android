e26f5dc447a9b1e40f2876e7f3ca6feeadb1dc25
kei-1111 <kei_02_1111@icloud.com>
2025-11-27 23:35:52 +0900

    chore: ViewerScreenの改良とPreviewの追加

--- feature/viewer/src/main/kotlin/io/github/kei_1111/newsflow/android/feature/viewer/ViewerScreen.kt
+++ feature/viewer/src/main/kotlin/io/github/kei_1111/newsflow/android/feature/viewer/ViewerScreen.kt
@@ -1,9 +1,11 @@
 package io.github.kei_1111.newsflow.android.feature.viewer
 
 import android.content.Intent
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -11,10 +13,17 @@ import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.datasource.CollectionPreviewParameterProvider
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import io.github.kei_1111.newsflow.android.core.designsystem.BuildConfig
 import io.github.kei_1111.newsflow.android.core.designsystem.component.feature.ErrorContent
 import io.github.kei_1111.newsflow.android.core.designsystem.component.feature.LoadingContent
+import io.github.kei_1111.newsflow.android.core.designsystem.theme.NewsflowAndroidTheme
+import io.github.kei_1111.newsflow.android.core.ui.preview.ScreenPreviews
 import io.github.kei_1111.newsflow.android.feature.viewer.component.ViewerContent
+import io.github.kei_1111.newsflow.library.core.model.Article
+import io.github.kei_1111.newsflow.library.core.model.NewsflowError
 import io.github.kei_1111.newsflow.library.feature.viewer.ViewerUiAction
 import io.github.kei_1111.newsflow.library.feature.viewer.ViewerUiEffect
 import io.github.kei_1111.newsflow.library.feature.viewer.ViewerUiState
@@ -72,29 +81,87 @@ private fun ViewerScreen(
     onUiAction: (ViewerUiAction) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Box(
-        modifier = modifier,
-        contentAlignment = Alignment.Center,
-    ) {
-        when (uiState) {
-            is ViewerUiState.Init, ViewerUiState.Loading -> {
-                LoadingContent()
-            }
+    Surface(modifier = modifier) {
+        AnimatedContent(
+            targetState = uiState,
+            label = "ViewerScreen",
+        ) { targetUiState ->
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center,
+            ) {
+                when (targetUiState) {
+                    is ViewerUiState.Init, ViewerUiState.Loading -> {
+                        LoadingContent()
+                    }
 
-            is ViewerUiState.Stable -> {
-                ViewerContent(
-                    uiState = uiState,
-                    onUiAction = onUiAction,
-                    modifier = Modifier.fillMaxSize()
-                )
-            }
+                    is ViewerUiState.Stable -> {
+                        ViewerContent(
+                            uiState = targetUiState,
+                            onUiAction = onUiAction,
+                            modifier = Modifier.fillMaxSize()
+                        )
+                    }
 
-            is ViewerUiState.Error -> {
-                ErrorContent(
-                    error = uiState.error,
-                    onClickActionButton = { onUiAction(ViewerUiAction.OnClickBackButton) }
-                )
+                    is ViewerUiState.Error -> {
+                        ErrorContent(
+                            error = targetUiState.error,
+                            onClickActionButton = { onUiAction(ViewerUiAction.OnClickBackButton) }
+                        )
+                    }
+                }
             }
         }
     }
 }
+
+@Composable
+@ScreenPreviews
+private fun ViewerScreenPreview(
+    @PreviewParameter(ViewerScreenPPP::class) parameter: ViewerScreenPreviewParameter
+) {
+    NewsflowAndroidTheme {
+        ViewerScreen(
+            uiState = parameter.uiState,
+            onUiAction = {},
+        )
+    }
+}
+
+private data class ViewerScreenPreviewParameter(
+    val uiState: ViewerUiState,
+)
+
+private class ViewerScreenPPP : CollectionPreviewParameterProvider<ViewerScreenPreviewParameter> (
+    collection = listOf(
+        ViewerScreenPreviewParameter(
+            uiState = ViewerUiState.Init,
+        ),
+        ViewerScreenPreviewParameter(
+            uiState = ViewerUiState.Loading,
+        ),
+        ViewerScreenPreviewParameter(
+            uiState = ViewerUiState.Stable(
+                viewingArticle = Article(
+                    id = "2135641799",
+                    source = "Politico",
+                    author = "Will Knight",
+                    title = "Amazon Is Building a Mega AI Supercomputer With Anthropic",
+                    description = """
+                        At its Re:Invent conference, 
+                        Amazon also announced new tools to help customers build generative AI programs, 
+                        including one that checks whether a chatbot's outputs are accurate or not.
+                    """.trimIndent(),
+                    url = "https://www.wired.com/story/amazon-reinvent-anthropic-supercomputer/",
+                    imageUrl = "${BuildConfig.DRAWABLE_PATH}/img_article_card_preview.png",
+                    publishedAt = 1763726640000,
+                )
+            )
+        ),
+        ViewerScreenPreviewParameter(
+            uiState = ViewerUiState.Error(
+                error = NewsflowError.InternalError.ArticleNotFound("Article Not Found")
+            )
+        )
+    )
+)
