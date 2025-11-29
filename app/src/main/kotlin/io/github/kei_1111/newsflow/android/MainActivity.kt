fde2ec3df224c46538686f41143f9d4b386b76cf
kei-1111 <kei_02_1111@icloud.com>
2025-11-29 18:42:23 +0900

    refactor: detekt警告の修正

--- app/src/main/kotlin/io/github/kei_1111/newsflow/android/MainActivity.kt
+++ app/src/main/kotlin/io/github/kei_1111/newsflow/android/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Surface
 import io.github.kei_1111.newsflow.android.core.designsystem.theme.NewsflowAndroidTheme
 import io.github.kei_1111.newsflow.android.core.ui.provider.DebouncedClickProvider
 import io.github.kei_1111.newsflow.android.navigation.NewsflowNavDisplay
