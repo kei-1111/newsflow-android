38822237bd1de0fae7048cdad644ff34973ac80f
kei-1111 <kei_02_1111@icloud.com>
2025-12-04 13:04:49 +0900

    chore: :feature:searchを作成

---
+++ feature/search/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    alias(libs.plugins.newsflow.android.android.feature)
+}
+
+android {
+    namespace = "io.github.kei_1111.newsflow.android.feature.search"
+}
+
+dependencies {
+    // Add your dependencies here
+}
