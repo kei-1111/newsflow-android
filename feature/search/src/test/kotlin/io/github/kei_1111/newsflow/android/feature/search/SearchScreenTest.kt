4e8bbfc0894fe482d9bb05206f8f78c94a1244fc
kei-1111 <kei_02_1111@icloud.com>
2025-12-18 18:47:45 +0900

    chore: detekt警告の修正

--- feature/search/src/test/kotlin/io/github/kei_1111/newsflow/android/feature/search/SearchScreenTest.kt
+++ feature/search/src/test/kotlin/io/github/kei_1111/newsflow/android/feature/search/SearchScreenTest.kt
@@ -11,7 +11,6 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.annotation.GraphicsMode
 
 @RunWith(NewsflowTestRunner::class)
 class SearchScreenTest {
--- feature/viewer/src/test/kotlin/io/github/kei_1111/newsflow/android/feature/viewer/ViewerScreenTest.kt
+++ feature/viewer/src/test/kotlin/io/github/kei_1111/newsflow/android/feature/viewer/ViewerScreenTest.kt
@@ -10,7 +10,6 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.annotation.GraphicsMode
 
 @RunWith(NewsflowTestRunner::class)
 class ViewerScreenTest {
